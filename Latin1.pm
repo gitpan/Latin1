a0ebacb4ab5e5d64e499bb8cb5b915533fd860d1
INABA Hitoshi <ina@cpan.org>
2011-01-26 13:18:56 +0000

    Import of INA/Latin1-0.71 from CPAN.
    gitpan-cpan-distribution: Latin1
    gitpan-cpan-version:      0.71
    gitpan-cpan-path:         INA/Latin1/Latin1-0.71.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Latin1.pm
+++ Latin1.pm
@@ -20,7 +20,7 @@ use Elatin1;
 
 BEGIN { eval q{ use vars qw($VERSION) } }
 
-$VERSION = sprintf '%d.%02d', q$Revision: 0.70 $ =~ m/(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.71 $ =~ m/(\d+)/oxmsg;
 
 # poor Symbol.pm - substitute of real Symbol.pm
 BEGIN {
@@ -39,14 +39,10 @@ BEGIN {
 # in Chapter 29: Functions
 # of ISBN 0-596-00027-8 Programming Perl Third Edition.
 
-unless (eval q{ use Fcntl qw(:flock); 1 }) {
-    eval q{
-        sub LOCK_SH {1}
-        sub LOCK_EX {2}
-        sub LOCK_UN {8}
-        sub LOCK_NB {4}
-    };
-}
+sub LOCK_SH() {1}
+sub LOCK_EX() {2}
+sub LOCK_UN() {8}
+sub LOCK_NB() {4}
 
 # P.707 29.2.33. exec
 # in Chapter 29: Functions
@@ -2739,9 +2735,7 @@ sub e_qr {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -2836,7 +2830,7 @@ sub e_qr_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3085,9 +3079,7 @@ sub e_s1 {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3183,7 +3175,7 @@ sub e_s1_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3548,9 +3540,7 @@ sub e_split {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3644,7 +3634,7 @@ sub e_split_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -4271,9 +4261,6 @@ as in the old byte-oriented mode.
 It is impossible. Because the following time is necessary.
 
 (1) Time of escape script for old byte-oriented perl.
-
-(2) Time of processing regular expression by escaped script while
-    multibyte anchoring.
 
 =item Goal #4:
 
